96d54c028114b2956da7404ab82a9b73d1dbcaea
jmv21 <78463691+jmv21@users.noreply.github.com>
2021-06-11 15:59:37 -0400

    Update forms.py

--- Cinema/forms.py
+++ Cinema/forms.py
@@ -1,11 +1,1 @@
-from django import forms
 
-
-class DiscountForm(forms.Form):
-    type = forms.CharField()
-    amount = forms.IntegerField()
-    description = forms.TextInput()
-    active = forms.BooleanField()
-
-
-DiscountFormSet = forms.formset_factory(DiscountForm)
