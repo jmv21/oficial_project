7d882d434491b3bbf386c582a5e2695118498a4e
jmv21 <78463691+jmv21@users.noreply.github.com>
2021-06-15 13:26:25 -0400

    Update settings.py

--- IS_Project/settings.py
+++ IS_Project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y+rojqx2h#pgdv_cg_nd0uu9%)dn3muq3!+%5&d^-i=t6ukx=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', '*']
 
 # Application definition
 
